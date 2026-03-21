938f23b7eaa237d5dce0e03308f633b1a8ae43c4
dheerajjha <dheerajjha@gmail.com>
2026-03-22 01:49:52 +0530

    Initial Commit

---
+++ structured Watch App/structuredApp.swift
@@ -0,0 +1,17 @@
+//
+//  structuredApp.swift
+//  structured Watch App
+//
+//  Created by Ranju Jha on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct structured_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ structured/structuredApp.swift
@@ -0,0 +1,17 @@
+//
+//  structuredApp.swift
+//  structured
+//
+//  Created by Ranju Jha on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct structuredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
